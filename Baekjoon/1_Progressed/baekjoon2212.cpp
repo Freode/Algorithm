5edeb8a29d2d2c51c057581a92efc2d5b79ac2b8
Freode <free4760@naver.com>
2024-03-18 23:15:09 +0900

    Solved Problem Baekjoon11399

---
+++ Baekjoon/1_Progressed/baekjoon2212.cpp
@@ -0,0 +1,19 @@
+// 골드 5 - 2212번 : 센서
+// 작성자 : free4760(jeonghoe22)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    // 입출력 최적화
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+
+}
