3b9dd86ca980e769de328dce18a5a4234e8355c3
Freode <free4760@naver.com>
2025-02-26 18:10:09 +0900

    Solved Programmers Problem
    1. Lv.2 - 87946번 : 피로도

---
+++ Baekjoon/1_Progressed/baekjoon1553.cpp
@@ -0,0 +1,14 @@
+// 골드 5 - 1553번 : 도미노 찾기
+// 작성자 : free4760(jeonghoe22)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    
+}
