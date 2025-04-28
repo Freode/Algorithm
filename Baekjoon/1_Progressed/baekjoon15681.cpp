b0fae04231ec3955128e2197a578d69e736f41c3
Freode <free4760@naver.com>
2025-04-28 12:49:50 +0900

    해결) 골드 3 - 17386번 : 선분 교차 1

---
+++ Baekjoon/1_Progressed/baekjoon15681.cpp
@@ -0,0 +1,18 @@
+// 골드 5 - 15681번 : 트리와 쿼리
+// 작성자 : free4760(jeonghoe22)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    // 입출력 최적화
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+
+}
