898a9db475d1c4edf51bd10891fbb8cc1c7c9169
Freode <free4760@naver.com>
2025-05-06 17:00:57 +0900

    해결) 골드 5 - 9084번 : 동전

---
+++ Baekjoon/1_Progressed/baekjoon15811.cpp
@@ -0,0 +1,18 @@
+// 골드 4 - 15811번 : 복면산?!
+// 작성자 : free4760(jeonghoe22)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    // 입출력 최적화
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+
+}
