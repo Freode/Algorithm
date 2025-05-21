31850f939e18b2e3c7fdd93f1faf233138a2cee6
Freode <free4760@naver.com>
2025-05-21 15:09:52 +0900

    해결) 브론즈 3 - 31798번 : 단원평가

---
+++ Baekjoon/1_Progressed/baekjoon14846.cpp
@@ -0,0 +1,19 @@
+// 골드 4 - 14846번 : 직사각형과 쿼리
+// 작성자 : free4760(jeonghoe22)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    // 입출력 최적화
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+
+
+}
