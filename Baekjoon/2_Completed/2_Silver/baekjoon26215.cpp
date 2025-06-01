8e7a6e746009a480813b8de25c9b54e5edf6a945
Freode <free4760@naver.com>
2025-06-01 22:43:40 +0900

    해결) 실버 3 - 26215번 : 눈 치우기

---
+++ Baekjoon/2_Completed/2_Silver/baekjoon26215.cpp
@@ -0,0 +1,63 @@
+// 실버 3 - 26215번 : 눈 치우기
+// 작성자 : free4760(jeonghoe22)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 내림차순 정렬
+// 가장 많은 눈이 쌓인 두 집을 청소
+
+vector<int> g_snows;
+
+void inputData(const int N)
+{
+    g_snows = vector<int>(N);
+
+    for(int& snow : g_snows)
+        cin >> snow;
+}
+
+int simulate(const int N)
+{ 
+    int result = 0;
+
+    sort(g_snows.begin(), g_snows.end(), greater<int>());
+
+    while(g_snows[0] != 0)
+    {
+        if(N != 1 && g_snows[1] != 0)
+            g_snows[1]--;
+
+        g_snows[0]--;
+        result++;
+
+        sort(g_snows.begin(), g_snows.end(), greater<int>());
+
+        // 24시간을 초과한 경우, -1을 출력
+        if(result > 1440)
+        {
+            result = -1;
+            break;
+        }
+    }
+
+    return result;
+}
+
+int main()
+{
+    // 입출력 최적화
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // 집의 수 N
+    int N;
+    cin >> N;
+
+    inputData(N);
+    cout << simulate(N) << "\n";
+}
