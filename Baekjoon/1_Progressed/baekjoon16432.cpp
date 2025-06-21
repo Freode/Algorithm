d6e9cb633db191c201aa31311fbb11cada7a5edd
Freode <free4760@naver.com>
2025-06-21 16:22:51 +0900

    해결) 골드 4 - 16432번 : 떡장수와 호랑이

---
+++ Baekjoon/1_Progressed/baekjoon16432.cpp
@@ -0,0 +1,20 @@
+// 골드 4 - 16432번 : 떡장수와 호랑이
+// 작성자 : free4760(jeonghoe22)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>>;
+
+int main()
+{
+    // 입출력 최적화
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+
+}
